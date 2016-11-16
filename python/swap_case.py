f5b00e0d0d4074cd1d9c39dcb9f14dac7fd707a0
Emese Billing <talebilling@gmail.com>
2016-11-16 10:02:52 +0100

    changed to functions

--- python/swap_case.py
+++ python/swap_case.py
@@ -1,19 +1,30 @@
 '''
 changing the lower to upper and vica versa
+input: HackerRank.com presents "Pythonist 2".
 '''
-my_string = input()
+
+
+def get_input():
+    my_string = input()
+    return my_string
+
 
 # solution 1
-new_string = my_string.swapcase()
-print(new_string)
+def swap_one(string):
+    new_string = string.swapcase()
+    return new_string
 
 
 # solution 2
-def swap(string):
+def swap_two(string):
     return string.swapcase()
-print(swap(my_string))
 
 
 # solution 3
 fvn = lambda string: string.swapcase()
+
+
+my_string = get_input()
+print(swap_one(my_string))
+print(swap_two(my_string))
 print(fvn(my_string))
