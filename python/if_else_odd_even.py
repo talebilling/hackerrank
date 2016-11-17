8ff6d36d5bcaf9e429f7f6e54051cb1f09c35e4e
Emese Billing <talebilling@gmail.com>
2016-11-17 18:18:27 +0100

    test ok, heki task

---
+++ python/if_else_odd_even.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+'''
+Task 
+Given an integer, , perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+'''
+
+import sys
+# input a number
+
+
+def get_input():
+    n = int(input().strip())
+    return n
+
+
+def check_given_number(n):
+    if n % 2 != 0:
+        print(weird)
+    if n % 2 == 0:
+        if n in range(2, 6) or 21 < n:
+            print(notwierd)
+        elif n in range(6, 22):
+            print(weird)
+
+
+weird = "Weird"
+notwierd = "Not Weird"
+n = get_input()
+check_given_number(n)
