47f415580d6e8031db94b25de515728bac155cad
Emese Billing <talebilling@gmail.com>
2016-11-17 19:14:32 +0100

    add an author/email comment

--- python/if_else_odd_even.py
+++ python/if_else_odd_even.py
@@ -1,5 +1,5 @@
 '''
-Task 
+Hackerrank Task 
 Given an integer, , perform the following conditional actions:
 If  is odd, print Weird
 If  is even and in the inclusive range of  to , print Not Weird
