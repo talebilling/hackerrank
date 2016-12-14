c54efb7efa84cbb24a3a1fd1150160ee072a77e0
Emese Billing <talebilling@gmail.com>
2016-12-14 21:55:34 +0100

    functions added

--- python/find_the_second_largest_number.py
+++ python/find_the_second_largest_number.py
@@ -1,14 +1,28 @@
-# input:
-# 5
-# 2 3 6 6 5
+'''input:
+5
+2 3 6 6 5
+'''
 
-n = int(input())
-my_list = input().split()
-my_list = [int(item) for item in my_list]
-larger = max(my_list)
-second_larger = min(my_list)
 
-for i, item in enumerate(my_list):
-    if second_larger < item < larger:
-        second_larger = my_list[i]
-print(second_larger)
+def main():
+    my_list = get_input()
+    second_larger = find_second_larger(my_list)
+    print(second_larger)
+
+
+def get_input():
+    n = int(input())
+    my_list = input().split()
+    my_list = [int(item) for item in my_list]
+    return my_list
+
+
+def find_second_larger(my_list):
+    larger = max(my_list)
+    temp = min(my_list)
+    for i, item in enumerate(my_list):
+        if temp < item < larger:
+            temp = my_list[i]
+    return temp
+
+main()
